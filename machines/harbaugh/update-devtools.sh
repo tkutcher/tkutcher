2cee508ceadbd65960a8f6766b5e9006c0ec823c
tkutcher <tim@tkutcher.com>
2025-03-22 10:45:34 -0400

    task: Local machine py3.12

--- machines/harbaugh/update-devtools.sh
+++ machines/harbaugh/update-devtools.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
 
-
 # set up basic variables for where things are located
 DEVTOOLS_VENV_DIR="${HOME}/tk/lib/devtools/venv"
 REQUIREMENTS_PATH="${HOME}/tk/lib/tkutcher/machines/harbaugh/devtools.requirements.txt"
 
 # source our file with the environment variables for GitLab private 
 # repo authentication - this gives us the ASL_GITLAB_TECH_INDEX_URL var
-source "${HOME}/tk/env/anvilorsolutions-gitlab.env"
+if [[ -f "${HOME}/tk/env/asl-gitlab.env" ]]; then
+  source "${HOME}/tk/env/asl-gitlab.env"
+else
+  echo "No env file?"
+  exit 1
+fi
+
+if [[ -z "$ASL_TECH_GITLAB_INDEX_URL" ]]; then
+  echo "No ASL_TECH_GITLAB_INDEX_URL variable?"
+  exit 1
+fi
+
 
 # activate the virtual environment we want to update dependencies for
 source "${DEVTOOLS_VENV_DIR}/bin/activate"
@@ -18,10 +28,8 @@ pip install --upgrade pip
 # update dependencies from requirements.txt file
 pip install \
   --upgrade \
-  -r "${REQUIREMENTS_PATH}" \
-  --index-url "${ASL_GITLAB_TECH_INDEX_URL}"
-
+  -r "$REQUIREMENTS_PATH" \
+  --index-url "$ASL_TECH_GITLAB_INDEX_URL"
 
 echo "------------------"
-echo "done!"
-
+echo "done"
